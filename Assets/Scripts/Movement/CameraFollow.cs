36293dd7450375936bd1e3eee9f7cf6b1d2a2328
Nicholas Jordan <NicholasPhJordan@gmail.com>
2021-04-20 22:12:40 -0500

    Initial commit

---
+++ Assets/Scripts/Movement/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float cameraDistOffset = 10;
+    public Camera mainCamera;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerInfo = player.transform.transform.position;
+        mainCamera.transform.position = new Vector3(playerInfo.x, playerInfo.y + cameraDistOffset, playerInfo.z);
+    }
+}
---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gamePaused = false;
+    public GameObject pauseMenuUi;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
