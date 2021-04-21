5e9783d9f20c39ce5fd9c356e650d02ecdb87d0e
Nicholas Jordan <NicholasPhJordan@gmail.com>
2021-04-21 16:47:33 -0500

    Working on goal collision
    When player collides with goal:
    -pause game
    -bring up winMenue

---
+++ Assets/Scripts/UI/WinMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinMenu : MonoBehaviour
+{
+    public GameObject winMenuUi;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Win();
+        }
+    }
+
+    public void Win()
+    {
+        winMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        PauseMenu.gamePaused = true;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
