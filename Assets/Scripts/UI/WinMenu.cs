c59a20036be1efab93774f14a6e696cce2bb56c9
Nicholas Jordan <NicholasPhJordan@gmail.com>
2021-04-22 13:32:13 -0500

    Added an end game scene and finished the core game

--- Assets/Scripts/UI/WinMenu.cs
+++ Assets/Scripts/UI/WinMenu.cs
@@ -5,25 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class WinMenu : MonoBehaviour
 {
-    public GameObject winMenuUi;
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            Win();
-        }
-    }
-
-    public void Win()
-    {
-        winMenuUi.SetActive(true);
-        Time.timeScale = 0f;
-        PauseMenu.gamePaused = true;
-    }
-
     public void Restart()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 }
